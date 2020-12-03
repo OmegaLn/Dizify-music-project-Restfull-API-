b00d590e7ec469b9a9736c44ba86f86e0615e26b
OmegaLn <45671786+OmegaLn@users.noreply.github.com>
2020-12-03 09:38:21 +0100

    Add files via upload

---
+++ DizifyMusic-spring-boot/src/main/java/com/ynov/nantes/soap/repository/TitleRepository.java
@@ -0,0 +1,28 @@
+package com.ynov.nantes.soap.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.util.Assert;
+
+import com.ynov.nantes.soap.entity.Album;
+import com.ynov.nantes.soap.entity.Title;
+
+/**
+ * Extension du Repository CRUD pour ajouter une méthode métier.
+ * 
+ * @author Matthieu BACHELIER
+ * @since 2020-10
+ * @version 1.0
+ */
+public interface TitleRepository extends JpaRepository<Title, Integer> {
+
+   /// TODO les méthodes pertinents en plus des méthodes CRUD (autogénérées), type findByName
+
+    Title findTitleById(int id);
+    Title findTitleByDesignation(String designation);
+    List<Title> findTitleByAlbumId(int id);
+    List<Title> findTitlesByArtistId(int id);
+}
